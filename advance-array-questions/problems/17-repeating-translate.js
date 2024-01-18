66670877876747368707da03d8959d6137b958a7
harsh <hrrshpal@gmail.com>
2024-01-18 13:55:25 +0530

    solved all problems

--- advance-array-questions/problems/17-repeating-translate.js
+++ advance-array-questions/problems/17-repeating-translate.js
@@ -29,12 +29,28 @@ console.log(repeatingTranslate("her family flew to France"));   // "herer family
 */
 
 let repeatingTranslate = function(sentence) {
-    // Your code here
+    let arr = sentence.split(" ")
+    let modified = arr.map((word)=>{
+        return translateWord(word)
+    })
+    return modified.join(" ")
 };
 
-
 let translateWord = function(word) {
-    // Your code here
+    let vowels = "aeiou"
+    if(word.length<3){
+        return word
+    } else {
+        if(vowels.includes(word[word.length-1])){
+            return word+word
+        } else{
+            for(let i=word.length-1; i>=0; i--){
+                if(vowels.includes(word[i])){
+                    return word+word.slice(i)
+                }
+            }
+        }
+    }
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -43,4 +59,4 @@ try {
     module.exports = repeatingTranslate;
 } catch (e) {
     module.exports = null;
-}
+}
