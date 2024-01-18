0f538f720af2bb6e0d6967457f7850700a92ccaa
harsh <hrrshpal@gmail.com>
2024-01-18 12:51:11 +0530

    solved 11th problem

--- advance-array-questions/problems/11-has-three-vowels.js
+++ advance-array-questions/problems/11-has-three-vowels.js
@@ -6,21 +6,28 @@ contains at least three different vowels.
 Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
 methods.
 
-Examples:
-
 console.log(hasThreeVowels('delicious'));       //  true
 console.log(hasThreeVowels('bootcamp prep'));   //  true
 console.log(hasThreeVowels('bootcamp'));        //  false
 console.log(hasThreeVowels('dog'));             //  false
 console.log(hasThreeVowels('go home'));         //  false
-
 */
 
 let hasThreeVowels = function(string) {
-    // Your code here
+    let arr = string.split("")
+    let vowels = ['a','e','i','o','u']
+    let count=0
+    arr.forEach((char)=>{
+        if(vowels.includes(char)){
+            count++
+            vowels.splice(vowels.indexOf(char),1)
+        }
+    })
+    if(count>=3){
+        return true
+    }
+    return false
 };
-
-// Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -28,4 +35,4 @@ try {
     module.exports = hasThreeVowels;
 } catch (e) {
     module.exports = null;
-}
+}
